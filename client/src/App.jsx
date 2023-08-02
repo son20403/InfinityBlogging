0a64958b1664a16e3ce17aff249f51bc1d693fd5
Nguyen Truong Son <ntstruongson2k3@gmail.com>
2023-08-02 09:22:49 +0700

    comment App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Header from './pages/Header'
 import Footer from './pages/Footer'
-
+/*tesst*/ 
 function App() {
   return (
     <>
